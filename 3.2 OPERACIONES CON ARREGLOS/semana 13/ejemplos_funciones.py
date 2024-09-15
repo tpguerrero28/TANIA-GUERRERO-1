505c3270aa3777d58b95057a0bc0f6e621468139
tguerrero28 <tp.guerrerom@uea.edu.ec>
2024-09-15 16:10:05 -0500

    Deber de la semana 13

---
+++ 3.2 OPERACIONES CON ARREGLOS/semana 13/ejemplos_funciones.py
@@ -0,0 +1,16 @@
+# ejemplo de funciones de lista
+#def mostrar_elementos(lista):
+ #   for elemento in lista:
+  #      print(elemento)
+
+#nombres = ['Juan', 'Pedro', 'Pablo',22 ]
+#mostrar_elementos(nombres)
+
+
+#ejemplo de un diccionario
+def mostrar_elemento(diccionario)
+    for clave, valor in diccionario.items():
+        print(clave, valor)
+
+    nombres = ['Juan', 'Pedro', 'Pablo',22 ]
+    mostrar_elemento(nombres_edad)
